d6fe6fb64e091af42597c542d95258df0967381e
shubhamgolam <shubhamgolam10@gmail.com>
2022-02-06 23:03:39 +0530

    Commit with requests module

--- corsdetec.py
+++ corsdetec.py
@@ -1,4 +1,5 @@
 import argparse
+import requests
 
 
 #create the parser
@@ -8,6 +9,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--subdomain', type=str, help='Single or Multiple Subdomains with spaces', nargs='+')
 parser.add_argument('--list', type=str,help='Provide list of subdomains')
+parser.add_argument('--out', type=str,help='Provide a output file name')
 
 #parse the argument
 args = parser.parse_args()
@@ -21,9 +23,25 @@ if args.subdomain:
 #open the file  > read the subdomains and print them
 
 if args.list:
-    file = open(args.list)
-    next(file)
-    print(file.read())
-    file.close()
+    file = open(args.list,'r')
+    try:
+        create = open(args.out,'x')
+    except:
+        print("File already created. Do you want to overrite it?[yes/no]")
+        answer = input()
+        if answer == 'yes':
+            print('File overwritten')
+            create = open(args.out,'w')
+        elif answer == 'no':
+            print('Give some unique name :)')
+            pass
 
-    #deded
+    #make requets to Subdomains with fix origin
+
+    origonheader = {'Origin':'https://evil.com'}
+    nullorigin = {'Origin':'null'}
+
+    for x in file:
+        y = x.replace('\n','')
+        r = requests.get('https://'+y,origonheader["Origin"])
+        print(r.url)
