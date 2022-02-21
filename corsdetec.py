610332fea14a0d0e454b230a216bd00e9d0bc6bb
shubham <shubhamgolam10@gmail.com>
2022-02-21 14:00:26 +0530

    File write and file handling

--- corsdetec.py
+++ corsdetec.py
@@ -18,11 +18,11 @@ parser.add_argument('--out', type=str,help='Provide a output file name')
 #parse the argument
 args = parser.parse_args()
 
-def make_requests(*takeall):
-    #takeall is tuple of tuple ((a,zzz),) with file and (sub,) for subdomain
-    rrr = takeall[0] #take the first value from tuple
-    a = rrr[1]#take the file write object
+def make_requests(readfile):
 
+
+   #takeall is tuple of tuple ((a,zzz),) with file and (sub,) for subdomain
+   #take the first value from tuple
 
     originheader = {'Origin':'https://evil.com'}
     nullorigin = {'Origin':'null'}
@@ -34,70 +34,81 @@ def make_requests(*takeall):
 
 
     if args.list:
-        zzz = rrr[0]
-        for x in zzz:
-            y = x.replace('\n','')
-            a.write(y + '\n')
+        with open(args.out,'a') as a:
 
-            r = requests.get('https://'+y, proxies=proxies,headers=originheader,verify=False)
-            ACAO = r.headers.get('Access-Control-Allow-Origin')
-            ACAC = r.headers.get('Access-Control-Allow-Credentials')
+            for x in readfile:
+                print(x)
+                y = x.replace('\n','')
+                r = requests.get('https://'+y, proxies=proxies,headers=originheader,verify=False)
+                ACAO = r.headers.get('Access-Control-Allow-Origin')
+                ACAC = r.headers.get('Access-Control-Allow-Credentials')
 
-            checkcors(ACAO,ACAC,origin,a,y)
+                checkcors(ACAO,ACAC,origin,a,y)
 
 
     if args.subdomain:
-        r = requests.get('https://'+rrr[0],proxies=proxies,headers=originheader,verify=False)
+        r = requests.get('https://'+readfile,proxies=proxies,headers=originheader,verify=False)
         ACAO = r.headers.get('Access-Control-Allow-Origin')
         ACAC = r.headers.get('Access-Control-Allow-Credentials')
 
-        checkcors(ACAO,ACAC,origin,rrr,None)
+        checkcors(ACAO,ACAC,origin,None,readfile)
 
 
 
 def checkcors(ACAO,ACAC,origin,*giveme):
     #same tuple logic here
 
-    a= giveme[0]
+    a = giveme[0]
     domain = giveme[1]
 
 
     if ACAO == origin and ACAC is None:
         if args.list:
+            a.write(domain)
             a.write(f'ACAO is {ACAO}')
-            a.write('Only ACAO is reflected\n')
+            a.write('Only ACAO is reflected'+'\n')
 
         else:
+            print(domain)
             print(f'ACAO is {ACAO}')
             print('Only ACAO is reflected\n')
 
     elif ACAO == '*' and ACAC is None:
         if args.list:
-            a.write('Wildcart Supported\n')
+            a.write(domain)
+            a.write('Wildcart Supported'+'\n')
 
         else:
+            print(domain)
             print('Wildcart Supported\n')
 
     elif ACAO == '*' and ACAC == 'true':
         if args.list:
+            a.write(domain)
             a.write('ACAO is * and Creds is True' +'\n')
 
         else:
+            print(domain)
             print('ACAO is * and Creds is True\n')
 
     elif ACAO == origin and ACAC == 'true':
         if args.list:
+            a.write(domain)
             a.write('Possible CORS Misconfiguration'+'\n')
 
         else:
+            print(domain)
             print('Possible CORS Misconfiguration\n')
 
     else:
         if args.list:
+            a.write(domain)
             a.write('No reflection' + '\n' )
 
         else:
+            print(domain)
             print('No reflection\n')
+
 
 
 if args.subdomain:
@@ -109,7 +120,8 @@ if args.subdomain:
 
 #open the file  > read the subdomains and print them
 def list_action():
-        file = open(args.list,'r')
+        with open(args.list,'r') as f:
+            file = f.readlines()
         if not args.out:
             print('Please provide --out txt file')
             sys.exit()
@@ -120,12 +132,8 @@ def list_action():
                 if answer == 'yes':
                     print('File will be overwritten')
                     print('\n')
-                    create = open(args.out,'w')
-                    create.close()
+                    os.remove(args.out)
 
-                    a = open(args.out,'a')
-
-                    return file, a
 
                 elif answer == 'no':
                     print('Give some unique name :)')
@@ -133,7 +141,7 @@ def list_action():
                 else:
                     print('Please make your decision')
                     sys.exit()
-
+        return file
 
 
 if args.list:
